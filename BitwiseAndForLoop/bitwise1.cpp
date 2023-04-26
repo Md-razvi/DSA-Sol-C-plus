b49452343b3dca9de479eba8372dd06ba7fd951b
MudabirRaza <mraza1748@gmail.com>
2023-04-26 09:26:52 +0530

    New_Upload

--- BitwiseAndForLoop/bitwise1.cpp
+++ BitwiseAndForLoop/bitwise1.cpp
@@ -4,6 +4,13 @@ int main(){
  //bitwise operations
  int a=5;
  int b=6;
- int c=a&b;
- cout<<c;
+ //int c=a&b;
+ cout<<"a&b= "<< (a&b) <<endl;
+ cout<<"a|b= "<< (a|b) <<endl;
+ cout <<"a^b= "<<(a^b) <<endl;
+ cout <<"~a= "<<(~a)<<endl;
+ 
+
+
+
 }
